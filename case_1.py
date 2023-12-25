11f3e547fe1de20126a462afc9795335db9328bc
mal-elbakry18 <100995605+mal-elbakry18@users.noreply.github.com>
2023-12-25 19:52:34 +0200

    Version 1.2

--- case_1.py
+++ case_1.py
@@ -25,10 +25,10 @@ class Check_states():
         self.current_state = state_list[0]
 
     def check_red_state(self, tr, number_of_cars):
-        if tr == 30 and number_of_cars > 0:
+        if tr == 31 and number_of_cars > 0:
             self.tr = 0
             return "YELLOW"
-        elif tr == 30 and number_of_cars == 0:
+        elif tr == 31 and number_of_cars == 0:
             self.tr = 0
             # check_red_state(tr, number_of_cars)
             return "RED"
@@ -38,10 +38,10 @@ class Check_states():
     # keep record of the previous state
 
     def check_yellow_state(self, ty, p_state):
-        if ty == 3 and p_state == "RED":
+        if ty == 4 and p_state == "RED":
             self.ty = 0
             return "GREEN"
-        elif ty == 3 and p_state == "GREEN":
+        elif ty == 4 and p_state == "GREEN":
             self.ty = 0
             return "RED"
         else:
@@ -49,11 +49,11 @@ class Check_states():
 
     # Check for the number of cars passed
 
-    def check_green_state(self, tg, number_of_people, cars_passed):
-        if tg == 50 and number_of_people > 0:
+    def check_green_state(self, tg, number_of_people):
+        if tg == 51 and number_of_people > 0:
             self.tg = 0
             return "YELLOW"
-        elif (tg == 50 and number_of_people == 0) or cars_passed == 20:
+        elif (tg == 51 and number_of_people == 0) :
             self.tg = 0
             return "YELLOW"
         else:
@@ -72,7 +72,7 @@ class Check_states():
 
 class RED_STATE(Check_states):
     def switch(self, check_states_instances):
-        check_states_instances.p_states = state_list[1]
+        check_states_instances.p_state = state_list[1]
         check_states_instances.current_state = state_list[0]
 
 
@@ -80,17 +80,87 @@ class YELLOW_STATE(Check_states):
     def switch(self, check_states_instances):
         check_states_instances.current_state = state_list[1]
 
-        if check_states_instances.p_states == "RED":
+        if check_states_instances.p_state == "RED":
             check_states_instances.next_state = state_list[2]
 
-        elif check_states_instances.p_states == "GREEN":
+        elif check_states_instances.p_state == "GREEN":
             check_states_instances.next_state = state_list[0]
 
 
 class GREEN_STATE(Check_states):
     def switch(self, check_states_instances):
-        check_states_instances.p_states = state_list[1]
+        check_states_instances.p_state = state_list[1]
         check_states_instances.current_state = state_list[2]
 
 
 
+def main():
+    nb_of_cars = 20
+    nb_of_people = 10
+    state = state_list[1]
+    Check_states_obj = Check_states()
+    Red_state_instances = RED_STATE()
+    Yellow_state_instances = YELLOW_STATE()
+    Green_state_instances = GREEN_STATE()
+    while True:
+        for i in range(30):
+            Check_states_obj.tr += 1
+            Check_states_obj.check_red_state(Check_states_obj.tr, nb_of_cars)
+            print(f"Current time is {Check_states_obj.tr}")
+            time.sleep(1)
+            if nb_of_cars > 12 or nb_of_cars < 12:
+                if nb_of_cars == 0:
+                    break
+                nb_of_cars -= 1
+            if Check_states_obj.tr == 0 and nb_of_cars == 0:
+                i = 0
+        print(f"{Check_states_obj.current_state}")
+        print(f"Number of cars: {nb_of_cars}")
+        print("out of loop 1 ")
+        Yellow_state_instances.switch(Check_states_obj)
+        print(f"Previous state is:{Check_states_obj.p_state}")
+        print(f"Current state is:{Check_states_obj.current_state}")
+
+        for j in range(3):
+            Check_states_obj.ty += 1
+            Check_states_obj.check_yellow_state(Check_states_obj.ty, Check_states_obj.p_state)
+            print(f"Current time is {Check_states_obj.ty}")
+            time.sleep(1)
+        print("out of loop 2")
+        Green_state_instances.switch(Check_states_obj)
+        print(f"Current state is: {Check_states_obj.current_state}")
+        print(f"Previous state is:{Check_states_obj.p_state}")
+
+        for k in range(50):
+            Check_states_obj.tg += 1
+            Check_states_obj.check_green_state(Check_states_obj.tg, nb_of_people)
+            print(f"Current time is {Check_states_obj.tg}")
+            time.sleep(1)
+            if nb_of_people >= 12:
+                nb_of_people -= 1
+
+        print("out of loop 3")
+
+        Yellow_state_instances.switch(Check_states_obj)
+        print(f"Current state is:{Check_states_obj.current_state}")
+        print(f"Previous state is:{Check_states_obj.p_state}")
+
+        for j in range(3):
+            Check_states_obj.ty += 1
+            Check_states_obj.check_yellow_state(Check_states_obj.ty, Check_states_obj.p_state)
+            print(f"Current time is {Check_states_obj.ty}")
+            time.sleep(1)
+        print("out of loop 4")
+
+        Red_state_instances.switch(Check_states_obj)
+        print(f"Current state is: {Check_states_obj.current_state}")
+        print(f"Previous state is:{Check_states_obj.p_state}")
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
